eb93b788d205aa2c4e513594745c97eae59c1d0e
JaekwonHa <hazxz1116@gmail.com>
2023-12-23 14:08:34 +0900

    init

---
+++ src/main/java/io/refactoring/buckpal/BuckPalApplication.java
@@ -0,0 +1,13 @@
+package io.refactoring.buckpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuckPalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuckPalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/refactoring/buckpal/BookHandsOnCleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package io.refactoring.buckpal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookHandsOnCleanArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
