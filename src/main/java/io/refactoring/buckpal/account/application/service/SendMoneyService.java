1263033a8b7353d16bac4c9e8d991e5267befb66
JaekwonHa <hazxz1116@gmail.com>
2023-12-24 11:19:18 +0900

    5장. 웹 어댑터 구현하기

--- src/main/java/io/refactoring/buckpal/account/application/service/SendMoneyService.java
+++ src/main/java/io/refactoring/buckpal/account/application/service/SendMoneyService.java
@@ -5,9 +5,13 @@ import io.refactoring.buckpal.account.application.port.in.SendMoneyUseCase;
 import io.refactoring.buckpal.account.application.port.out.AccountLock;
 import io.refactoring.buckpal.account.application.port.out.LoadAccountPort;
 import io.refactoring.buckpal.account.application.port.out.UpdateAccountStatePort;
+import io.refactoring.buckpal.account.domain.Account;
+import io.refactoring.buckpal.account.domain.Account.AccountId;
 import io.refactoring.buckpal.common.UseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 @UseCase
@@ -19,10 +23,45 @@ public class SendMoneyService implements SendMoneyUseCase {
     private final UpdateAccountStatePort updateAccountStatePort;
     private final MoneyTransferProperties moneyTransferProperties;
 
-    private void checkThreshold(SendMoneyCommand command) {
-        if(command.getMoney().isGreaterThan(moneyTransferProperties.getMaximumTransferThreshold())){
-            throw new ThresholdExceededException(moneyTransferProperties.getMaximumTransferThreshold(), command.getMoney());
+    @Override
+    public boolean sendMoney(SendMoneyCommand command) {
+
+        checkThreshold(command);
+
+        LocalDateTime baselineDate = LocalDateTime.now().minusDays(10);
+
+        Account sourceAccount = loadAccountPort.loadAccount(
+                command.getSourceAccountId(),
+                baselineDate);
+
+        Account targetAccount = loadAccountPort.loadAccount(
+                command.getTargetAccountId(),
+                baselineDate);
+
+        AccountId sourceAccountId = sourceAccount.getId()
+                .orElseThrow(() -> new IllegalStateException("expected source account ID not to be empty"));
+        AccountId targetAccountId = targetAccount.getId()
+                .orElseThrow(() -> new IllegalStateException("expected target account ID not to be empty"));
+
+        accountLock.lockAccount(sourceAccountId);
+        if (!sourceAccount.withdraw(command.getMoney(), targetAccountId)) {
+            accountLock.releaseAccount(sourceAccountId);
+            return false;
         }
+
+        accountLock.lockAccount(targetAccountId);
+        if (!targetAccount.deposit(command.getMoney(), sourceAccountId)) {
+            accountLock.releaseAccount(sourceAccountId);
+            accountLock.releaseAccount(targetAccountId);
+            return false;
+        }
+
+        updateAccountStatePort.updateActivities(sourceAccount);
+        updateAccountStatePort.updateActivities(targetAccount);
+
+        accountLock.releaseAccount(sourceAccountId);
+        accountLock.releaseAccount(targetAccountId);
+        return true;
     }
 
     @Override
